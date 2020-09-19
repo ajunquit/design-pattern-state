a664a0c3afe9a8a434abfc051f12ce0550c07a18
ajunquit <ajunquit@gmail.com>
2020-09-19 13:28:27 -0500

    first commit

---
+++ PatternState.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pattern_state
+{
+    public class TheProblem
+    {
+        public IState CustomState { get; set; }
+
+        public TheProblem()
+        {
+            // initial solucion is the A
+            this.CustomState = new SolutionA();
+        }
+
+        public TheProblem(IState Solution)
+        {
+            // set the solucion by dependency inject
+            this.CustomState = Solution;
+        }
+
+        public void ChangeState(IState state)
+        {
+            this.CustomState = state;
+        }
+
+    }
+
+    public class SolutionA : IState
+    {
+
+        public void Handle()
+        {
+            Console.WriteLine("Solution A.");
+        }
+    }
+    public class SolutionB : IState
+    {
+        public void Handle()
+        {
+            Console.WriteLine("Solution B.");
+        }
+    }
+
+    public interface IState
+    {
+        void Handle();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace pattern_state
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // run any example
+            ExampleOne();
+            // ExampleTwo();
+            Console.ReadLine();
+        }
+
+        static void ExampleOne()
+        {
+            var context = new TheProblem();
+            context.CustomState.Handle();
+            context.ChangeState(new SolutionB());
+            context.CustomState.Handle();
+            
+        }
+
+        static void ExampleTwo()
+        {
+            var context = new TheProblem(new SolutionB());
+            context.CustomState.Handle();
+            context.ChangeState(new SolutionA());
+            context.CustomState.Handle();
+        }
+
+    }
+}
